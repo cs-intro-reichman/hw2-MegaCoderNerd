d201fe3acd96fd237913bb0103af2c994f877726
Omer <godmega67@gmail.com>
2024-11-13 18:41:41 +0200

    fixed cheers and calc pi

--- CalcPi.java
+++ CalcPi.java
@@ -16,6 +16,6 @@ public class CalcPi {
 			}
 		}
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated:     " + 4 * sum);
+		System.out.println("pi, approximated:    " + 4 * sum);
 	}
 }
